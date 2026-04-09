30766fb78166176beaa9afc995283d3938f43a24
Daniel Gading <2748839+dgading@users.noreply.github.com>
2026-04-09 14:22:27 -0400

    534ez add 2025 section 8 (#27360)
    * 534ez add 2025 section 8
    
    * Copy and update tests for section 8
    
    * Fix copilot issue

--- modules/survivors_benefits/lib/survivors_benefits/pdf_fill/sections/V2025/section_08.rb
+++ modules/survivors_benefits/lib/survivors_benefits/pdf_fill/sections/V2025/section_08.rb
@@ -8,8 +8,38 @@ module SurvivorsBenefits
       # Section VIII: Nursing Home or Increased Survivors Entitlement
       class Section8 < Section
         # Section configuration hash
-        KEY = {}.freeze
+        KEY = {
+          'claimingMonthlySpecialPension' => {
+            key: 'form1[0].#subform[156].RadioButtonList[27]'
+          },
+          'claimantLivesInANursingHomeYes' => {
+            key: 'form1[0].#subform[156].CheckboxYES[0]'
+          },
+          'claimantLivesInANursingHomeNo' => {
+            key: 'form1[0].#subform[156].CheckboxNO[0]'
+          }
+        }.freeze
+
         def expand(form_data = {})
+          form_data['claimingMonthlySpecialPension'] =
+            case form_data['claimingMonthlySpecialPension']
+            when true then '1'
+            when false then 'NO'
+            else 'Off'
+            end
+
+          case form_data['claimantLivesInANursingHome']
+          when true
+            form_data['claimantLivesInANursingHomeYes'] = '1'
+            form_data['claimantLivesInANursingHomeNo'] = 'Off'
+          when false
+            form_data['claimantLivesInANursingHomeYes'] = 'Off'
+            form_data['claimantLivesInANursingHomeNo'] = '1'
+          else
+            form_data['claimantLivesInANursingHomeYes'] = 'Off'
+            form_data['claimantLivesInANursingHomeNo'] = 'Off'
+          end
+
           form_data
         end
       end
