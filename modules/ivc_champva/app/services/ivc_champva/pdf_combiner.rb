909ecf3a793c3b3df13abcb25e8010a28624f7f1
Steve Long <2211897+stevelong00@users.noreply.github.com>
2026-04-21 11:04:09 -0400

    Added alternate combining routine using HexaPdf (#27758)
    * Added alternate combining routine using HexaPdf
    
    * Added logging to track which library is used
    
    * Fixed typo
    
    * PII fix and page order test update
    
    - Prevented logging of file name to protect PII that it may contain
    - Updated page order test to check text on each page instead of raw contents data
    
    * Added index to error logging

--- modules/ivc_champva/app/services/ivc_champva/pdf_combiner.rb
+++ modules/ivc_champva/app/services/ivc_champva/pdf_combiner.rb
@@ -7,16 +7,67 @@ module IvcChampva
 
     # @param merged_pdf_path [String] The path of the output file
     # @param file_paths [Array<String>] The paths to the PDFs to combine
-    # @param [User] current_user The current user, used for feature flags
+    # @param [User, nil] current_user The current user, used for feature flags
     # @return [String] The path to the combined PDF
     def self.combine(merged_pdf_path, file_paths, current_user = nil)
       return merged_pdf_path if file_paths.empty?
 
+      if Flipper.enabled?(:champva_use_hexapdf_to_combine_pdfs, current_user)
+        Rails.logger.info('PdfCombiner combining using HexaPDF')
+        combine_using_hexapdf(merged_pdf_path, file_paths)
+      else
+        Rails.logger.info('PdfCombiner combining using CombinePDF')
+        combine_using_combinepdf(merged_pdf_path, file_paths, current_user)
+      end
+    end
+
+    # Alternative PDF combiner using HexaPDF instead of CombinePDF.
+    #
+    # @param merged_pdf_path [String] The path of the output file
+    # @param file_paths [Array<String>] The paths to the PDFs to combine
+    # @return [String] The path to the combined PDF
+    def self.combine_using_hexapdf(merged_pdf_path, file_paths)
+      # Create a new empty HexaPDF document to hold the combined pages
+      target = HexaPDF::Document.new
+
+      file_paths.each_with_index do |file_path, index|
+        # Open each source PDF
+        source = HexaPDF::Document.open(file_path)
+
+        # Import each page from the source into the target document
+        source.pages.each do |page|
+          target.pages << target.import(page)
+        end
+      rescue SystemCallError => e # Base class for any filesystem related errors
+        # e.message could contain a filename and PII, so only pass on the decoded error number when available
+        error_name = Errno.constants.find(proc {
+          "Unknown #{e.errno}"
+        }) { |c| Errno.const_get(c).new.errno == e.errno }.to_s
+        Rails.logger.error("Error merging file at index #{index}: SystemCallError #{error_name}")
+        raise
+      rescue => e
+        Rails.logger.error("Error merging file at index #{index}: #{e.message}")
+        raise
+      end
+
+      # Write the combined document to the output path
+      target.write(merged_pdf_path, optimize: true)
+
+      merged_pdf_path
+    end
+
+    # Original PDF combiner using CombinePDF.
+    #
+    # @param merged_pdf_path [String] The path of the output file
+    # @param file_paths [Array<String>] The paths to the PDFs to combine
+    # @param [User, nil] current_user The current user, used for feature flags
+    # @return [String] The path to the combined PDF
+    def self.combine_using_combinepdf(merged_pdf_path, file_paths, current_user = nil)
       allow_optional_content = Flipper.enabled?(:champva_allow_pdf_optional_content, current_user)
 
       combined_pdf = CombinePDF.new
 
-      file_paths.each do |file_path|
+      file_paths.each_with_index do |file_path, index|
         pdf = CombinePDF.load(file_path, allow_optional_content:)
         pdf.pages.each do |page|
           # Store the source filename directly in the page hash
@@ -24,8 +75,15 @@ module IvcChampva
           page[:SourceFileName] = file_path
         end
         combined_pdf << pdf
+      rescue SystemCallError => e # Base class for any filesystem related errors
+        # e.message could contain a filename and PII, so only pass on the decoded error number when available
+        error_name = Errno.constants.find(proc {
+          "Unknown #{e.errno}"
+        }) { |c| Errno.const_get(c).new.errno == e.errno }.to_s
+        Rails.logger.error("Error merging file at index #{index}: SystemCallError #{error_name}")
+        raise
       rescue => e
-        Rails.logger.error("Error merging #{file_path}: #{e.message}")
+        Rails.logger.error("Error merging file at index #{index}: #{e.message}")
         raise
       end
 
--- modules/ivc_champva/spec/services/pdf_combiner_spec.rb
+++ modules/ivc_champva/spec/services/pdf_combiner_spec.rb
@@ -9,6 +9,9 @@ describe IvcChampva::PdfCombiner do
     @form_path = 'modules/ivc_champva/spec/fixtures/pdfs/vha_10_10d-filled.pdf'
     @merged_path = 'modules/ivc_champva/spec/fixtures/pdfs/vha_10_10d-merged.pdf'
     @image_path = 'modules/ivc_champva/spec/fixtures/images/test_image.pdf'
+    @image_path_non_existent = 'modules/ivc_champva/spec/fixtures/images/test_image_non_existent.pdf'
+    @image_path_encrypted =
+      'modules/ivc_champva/spec/fixtures/images/test-image-encrypted-with-lost-password.pdf'
     @doctors_note_path = 'spec/fixtures/files/doctors-note.pdf'
   end
 
@@ -18,58 +21,94 @@ describe IvcChampva::PdfCombiner do
   end
 
   describe '#combine' do
-    it 'does not combine if there are no additional files' do
-      expect(IvcChampva::PdfCombiner.combine(@merged_path, [])).to eq(@merged_path)
-      expect(File).not_to exist(@merged_path)
-    end
-
-    it 'combines PDF files' do
-      pages = [@form_path, @image_path]
-      expect(IvcChampva::PdfCombiner.combine(@merged_path, pages)).to eq(@merged_path)
-
-      # Check that the merged file has the correct number of pages
-      merged_pdf = CombinePDF.load(@merged_path)
-      expect(merged_pdf.pages.count).to eq(pages.count)
-    end
-
-    it 'combines PDF files and maintains page order' do
-      pages = [@form_path, @image_path, @doctors_note_path]
-      expect(IvcChampva::PdfCombiner.combine(@merged_path, pages)).to eq(@merged_path)
-
-      merged_pdf = CombinePDF.load(@merged_path)
-      expect(merged_pdf.pages.count).to eq(pages.count)
-
-      # ensure order matches pages array
-      merged_pdf.pages.each_with_index do |page, index|
-        expect(page[:SourceFileName]).to eq(pages[index])
-      end
-    end
-
-    context 'when combining PDFs with optional content' do
-      let(:optional_content_pdf_path) { 'modules/ivc_champva/spec/fixtures/pdfs/pdf_with_optional_content.pdf' }
-
-      context 'when champva_allow_pdf_optional_content is enabled' do
+    [true, false].each do |use_hexapdf|
+      context "when champva_use_hexapdf_to_combine_pdfs is #{use_hexapdf}" do
         before do
-          allow(Flipper).to receive(:enabled?).with(:champva_allow_pdf_optional_content, anything).and_return(true)
+          allow(Flipper).to receive(:enabled?).with(:champva_use_hexapdf_to_combine_pdfs,
+                                                    anything).and_return(use_hexapdf)
         end
 
-        it 'combines PDF with optional content successfully' do
-          pages = [optional_content_pdf_path]
-          expect(described_class.combine(@merged_path, pages)).to eq(@merged_path)
-
-          merged_pdf = CombinePDF.load(@merged_path, allow_optional_content: true)
-          expect(merged_pdf.pages.count).to eq(1)
-        end
-      end
-
-      context 'when champva_allow_pdf_optional_content is disabled' do
-        before do
-          allow(Flipper).to receive(:enabled?).with(:champva_allow_pdf_optional_content, anything).and_return(false)
+        it 'does not combine if there are no additional files' do
+          expect(IvcChampva::PdfCombiner.combine(@merged_path, [])).to eq(@merged_path)
+          expect(File).not_to exist(@merged_path)
         end
 
-        it 'raises an error when combining PDF with optional content' do
-          pages = [optional_content_pdf_path]
-          expect { described_class.combine(@merged_path, pages) }.to raise_error(StandardError)
+        it 'combines PDF files' do
+          pages = [@form_path, @image_path]
+          expect(IvcChampva::PdfCombiner.combine(@merged_path, pages)).to eq(@merged_path)
+
+          # Check that the merged file has the correct number of pages
+          merged_pdf = CombinePDF.load(@merged_path)
+          expect(merged_pdf.pages.count).to eq(pages.count)
+        end
+
+        it 'combines PDF files and maintains page order' do
+          pages = [@form_path, @image_path, @doctors_note_path]
+          expect(IvcChampva::PdfCombiner.combine(@merged_path, pages)).to eq(@merged_path)
+
+          merged_pages = PDF::Reader.new(@merged_path).pages
+          expect(merged_pages.count).to eq(pages.count)
+
+          # ensure text on each page matches the corresponding source file
+          merged_pages.each_with_index do |page, index|
+            source_text = PDF::Reader.new(pages[index]).pages[0].text
+            expect(page.text).to eq(source_text)
+          end
+        end
+
+        it 'fails to combine a PDF file that does not exist and does not leak PII' do
+          pages = [@form_path, @image_path_non_existent]
+          allow(Rails.logger).to receive(:error)
+
+          expect { IvcChampva::PdfCombiner.combine(@merged_path, pages) }.to raise_error(SystemCallError)
+
+          expect(Rails.logger).to have_received(:error).with(a_string_matching(/Error merging file at index/)) do |msg|
+            expect(msg).not_to include(@image_path_non_existent)
+          end
+        end
+
+        it 'fails to combine a PDF file that is encrypted and does not leak PII' do
+          pages = [@form_path, @image_path_encrypted]
+          allow(Rails.logger).to receive(:error)
+
+          expect { IvcChampva::PdfCombiner.combine(@merged_path, pages) }.to raise_error(StandardError)
+
+          expect(Rails.logger).to have_received(:error).with(a_string_matching(/Error merging file at index/)) do |msg|
+            expect(msg).not_to include(@image_path_encrypted)
+          end
+        end
+
+        context 'when combining PDFs with optional content' do
+          let(:optional_content_pdf_path) { 'modules/ivc_champva/spec/fixtures/pdfs/pdf_with_optional_content.pdf' }
+
+          context 'when champva_allow_pdf_optional_content is enabled' do
+            before do
+              allow(Flipper).to receive(:enabled?).with(:champva_allow_pdf_optional_content, anything).and_return(true)
+            end
+
+            it 'combines PDF with optional content successfully' do
+              pages = [optional_content_pdf_path]
+              expect(described_class.combine(@merged_path, pages)).to eq(@merged_path)
+
+              merged_pdf = CombinePDF.load(@merged_path, allow_optional_content: true)
+              expect(merged_pdf.pages.count).to eq(1)
+            end
+          end
+
+          context 'when champva_allow_pdf_optional_content is disabled' do
+            before do
+              allow(Flipper).to receive(:enabled?).with(:champva_allow_pdf_optional_content, anything).and_return(false)
+            end
+
+            it 'raises an error when combining PDF with optional content only when not using HexaPdf' do
+              pages = [optional_content_pdf_path]
+              if use_hexapdf
+                expect { described_class.combine(@merged_path, pages) }.not_to raise_error
+              else
+                expect { described_class.combine(@merged_path, pages) }.to raise_error(StandardError)
+              end
+            end
+          end
         end
       end
     end
