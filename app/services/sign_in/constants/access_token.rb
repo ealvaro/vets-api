d512c5cb24a79669c1b5e3a54a8ecf965f8ad10d
Nguyen, Emily K. (Oddball, Inc.) <EMILY.NGUYEN21@va.gov>
2026-05-13 15:44:49 -0500

    Update issuer standard format as url (#28227)

--- app/services/sign_in/constants/access_token.rb
+++ app/services/sign_in/constants/access_token.rb
@@ -11,7 +11,7 @@ module SignIn
         CURRENT_VERSION = 'V0'
       ].freeze
 
-      ISSUER = 'va.gov sign in'
+      ISSUER = IdentitySettings.sign_in.issuer
       USER_ATTRIBUTES = %w[first_name last_name email all_emails].freeze
     end
   end
