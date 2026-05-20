a23fc2be9d87adae132abeedfee097949045b438
Cassity, Rachal C. (QUANTUM MANAGEMENT, LLC) <Rachal.Cassity@va.gov>
2026-05-20 08:12:55 -0500

    Remove redundant plain-text logger that was causing stacktraces to appear as individual log events in Datadog (#28356)
    Co-authored-by: Rachal.Cassity <rachal.cassity@oddball.io>

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,18 +112,9 @@ Rails.application.configure do
   # Tell Active Support which deprecation messages to disallow.
   config.active_support.disallowed_deprecation_warnings = []
 
-  # Use default logging formatter so that PID and timestamp are not suppressed.
-  config.log_formatter = Logger::Formatter.new
-
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
-
-  # Log to standard out, with specified formatter
-  $stdout.sync = config.autoflush_log
-  logger = ActiveSupport::Logger.new($stdout)
-  logger.formatter = config.log_formatter
-  config.logger = ActiveSupport::TaggedLogging.new(logger)
 
   # Do not dump schema after migrations.
   # config.active_record.dump_schema_after_migration = false
