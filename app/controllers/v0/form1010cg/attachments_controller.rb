1270b857bbc145450072d00b6463c525ec3114c5
Tolmach, Rebecca M. (QUANTUM MANAGEMENT, LLC) <Rebecca.Tolmach@va.gov>
2026-06-04 17:37:23 -0400

    Revert "Implement PDF decryption handling and error responses (#28566)" (#28651)
    This reverts commit d77b92540a4f0872aef365b9b4e5ee35efd89a71.

--- app/controllers/v0/form1010cg/attachments_controller.rb
+++ app/controllers/v0/form1010cg/attachments_controller.rb
@@ -6,10 +6,7 @@ module V0
       include FormAttachmentCreate
       service_tag 'caregiver-application'
 
-      DECRYPTION_ERROR_MESSAGE = 'The password you entered is incorrect. Please try again.'
-
       skip_before_action :authenticate, raise: false
-      rescue_from Common::Exceptions::UnprocessableEntity, with: :handle_unprocessable_entity
 
       FORM_ATTACHMENT_MODEL = ::Form1010cg::Attachment
 
@@ -17,25 +14,6 @@ module V0
 
       def serializer_klass
         ::Form1010cg::AttachmentSerializer
-      end
-
-      def handle_unprocessable_entity(exception)
-        is_pdf_unlock_error = exception.errors.first&.source == 'Common::PdfHelpers.unlock_pdf'
-
-        if is_pdf_unlock_error
-          Rails.logger.info(
-            '[Form 10-10CG] Attachment decryption failed',
-            encrypted: true,
-            source: exception.errors.first&.source
-          )
-
-          render json: {
-            errorMessage: DECRYPTION_ERROR_MESSAGE,
-            isEncrypted: true
-          }, status: :unprocessable_entity
-        else
-          render_errors(exception)
-        end
       end
     end
   end
--- app/models/form1010cg/attachment.rb
+++ app/models/form1010cg/attachment.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'common/pdf_helpers'
-
 module Form1010cg
   class Attachment < FormAttachment
     ATTACHMENT_UPLOADER_CLASS = ::Form1010cg::PoaUploader
@@ -13,37 +11,6 @@ module Form1010cg
       File.write(local_path, remote_file.read)
 
       local_path
-    end
-
-    private
-
-    def unlock_pdf(file, file_password)
-      tmpf = Tempfile.new(['decrypted_form1010cg_attachment', '.pdf'])
-      begin
-        Common::PdfHelpers.unlock_pdf(file.tempfile.path, file_password, tmpf.path)
-      rescue Common::Exceptions::UnprocessableEntity => e
-        tmpf.unlink
-        handle_pdf_unlock_error(e)
-      end
-      file.tempfile.unlink
-      file.tempfile = tmpf
-      file
-    end
-
-    def handle_pdf_unlock_error(error)
-      detail = error.errors&.first&.detail
-      if detail == I18n.t('errors.messages.uploads.pdf.incorrect_password')
-        Rails.logger.warn('[Form 10-10CG] PDF unlock failed: incorrect password provided')
-        safe_detail = I18n.t('errors.messages.uploads.pdf.incorrect_password')
-      else
-        Rails.logger.warn('[Form 10-10CG] PDF unlock failed: invalid pdf provided')
-        safe_detail = I18n.t('errors.messages.uploads.pdf.invalid')
-      end
-
-      raise Common::Exceptions::UnprocessableEntity.new(
-        detail: safe_detail,
-        source: 'Common::PdfHelpers.unlock_pdf'
-      ), cause: nil
     end
   end
 end
