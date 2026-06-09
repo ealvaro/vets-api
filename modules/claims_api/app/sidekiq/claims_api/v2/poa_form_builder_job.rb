ccd019fa220d8f7504b5dc44a6fef23502bf8c29
Lamsal, Siddhartha (Deloitte) <Siddhartha.Lamsal@va.gov>
2026-06-09 09:42:07 -0400

    Api 56020 remove method length disable poa form builder (#28609)
    * API-56020: Remove Rubocop MethodLength disable from v2 POA form builder
    
    Extract build_and_upload_pdf and queue_next_job private methods from
    perform to bring it under the Metrics/MethodLength limit. Also remove
    a no-op dig call whose return value was unused.
    
    * rename

--- modules/claims_api/app/sidekiq/claims_api/v2/poa_form_builder_job.rb
+++ modules/claims_api/app/sidekiq/claims_api/v2/poa_form_builder_job.rb
@@ -14,7 +14,7 @@ module ClaimsApi
       # it queues a job to update the POA code in BGS, as well.
       #
       # @param power_of_attorney_id [String] Unique identifier of the submitted POA
-      def perform(power_of_attorney_id, form_number, action, rep_id) # rubocop:disable Metrics/MethodLength
+      def perform(power_of_attorney_id, form_number, action, rep_id)
         power_of_attorney = ClaimsApi::PowerOfAttorney.find(power_of_attorney_id)
 
         process = ClaimsApi::Process.find_or_create_by(processable: power_of_attorney,
@@ -23,18 +23,8 @@ module ClaimsApi
 
         rep = ::Veteran::Service::Representative.where(representative_id: rep_id).order(created_at: :desc).first
 
-        output_path = pdf_constructor(form_number).construct(data(power_of_attorney, form_number, rep),
-                                                             id: power_of_attorney.id)
-        power_of_attorney.form_data.dig('serviceOrganization', 'poaCode')
-
-        doc_type = form_number == '2122' ? 'L190' : 'L075'
-        benefits_doc_upload(poa: power_of_attorney, pdf_path: output_path, doc_type:, action:)
-
-        if dependent_filing?(power_of_attorney)
-          ClaimsApi::PoaAssignDependentClaimantJob.perform_async(power_of_attorney.id, rep_id)
-        else
-          ClaimsApi::PoaUpdater.perform_async(power_of_attorney.id, rep_id)
-        end
+        build_and_upload_pdf(power_of_attorney, form_number, action, rep)
+        queue_poa_assignment(power_of_attorney, rep_id)
         process.update!(step_status: 'SUCCESS', error_messages: [], completed_at: Time.zone.now)
       rescue Errno::ENOENT
         rescue_file_not_found(power_of_attorney, process)
@@ -44,6 +34,21 @@ module ClaimsApi
       end
 
       private
+
+      def build_and_upload_pdf(power_of_attorney, form_number, action, rep)
+        output_path = pdf_constructor(form_number).construct(data(power_of_attorney, form_number, rep),
+                                                             id: power_of_attorney.id)
+        doc_type = form_number == '2122' ? 'L190' : 'L075'
+        benefits_doc_upload(poa: power_of_attorney, pdf_path: output_path, doc_type:, action:)
+      end
+
+      def queue_poa_assignment(power_of_attorney, rep_id)
+        if dependent_filing?(power_of_attorney)
+          ClaimsApi::PoaAssignDependentClaimantJob.perform_async(power_of_attorney.id, rep_id)
+        else
+          ClaimsApi::PoaUpdater.perform_async(power_of_attorney.id, rep_id)
+        end
+      end
 
       def benefits_doc_upload(poa:, pdf_path:, doc_type:, action:)
         PoaDocumentService.new.create_upload(poa:, pdf_path:, doc_type:, action:)
