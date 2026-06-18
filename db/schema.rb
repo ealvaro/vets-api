a234b1e6b944ac085bad0b1e68dea66814909fe7
Anderson, Riley D. (ODDCORE JV, LLC) <RILEY.ANDERSON@va.gov>
2026-06-18 15:32:09 -0600

    remove certificates column from client and service account configs (#28559)

--- db/schema.rb
+++ db/schema.rb
@@ -752,7 +752,6 @@ ActiveRecord::Schema[7.2].define(version: 2026_06_15_183404) do
     t.datetime "updated_at", null: false
     t.text "logout_redirect_uri"
     t.boolean "pkce"
-    t.string "certificates", default: [], array: true
     t.text "description"
     t.string "access_token_attributes", default: [], array: true
     t.text "terms_of_use_url"
@@ -1739,7 +1738,6 @@ ActiveRecord::Schema[7.2].define(version: 2026_06_15_183404) do
     t.text "scopes", default: [], null: false, array: true
     t.string "access_token_audience", null: false
     t.interval "access_token_duration", null: false
-    t.string "certificates", default: [], array: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "access_token_user_attributes", default: [], array: true
